cc258034b633e771f1dea211523a2b30e10dd72f
Anthony Robb <arobb.cloud@gmail.com>
2026-07-01 20:48:17 -0500

    Update for future configurations

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -75,18 +75,18 @@ output "private_subnet_b_id" {
 #}
 
 # RDS Outputs
-output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
-}
+#output "rds_endpoint" {
+#  value = aws_db_instance.postgres.endpoint
+#}
 
-output "rds_address" {
-  value = aws_db_instance.postgres.address
-}
+#output "rds_address" {
+#  value = aws_db_instance.postgres.address
+#}
 
-output "rds_port" {
-  value = aws_db_instance.postgres.port
-}
+#output "rds_port" {
+#  value = aws_db_instance.postgres.port
+#}
 
-output "rds_db_name" {
-  value = aws_db_instance.postgres.db_name
-}
+#output "rds_db_name" {
+#  value = aws_db_instance.postgres.db_name
+#}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,20 +30,20 @@ variable "owner" {
 #}
 
 # RDS Instance
-variable "db_name" {
-  description = "Initial PostgreSQL database name"
-  type        = string
-  default     = "appdb"
-}
+#variable "db_name" {
+#  description = "Initial PostgreSQL database name"
+#  type        = string
+ # default     = "appdb"
+#}
 
-variable "db_username" {
-  description = "Master username for PostgreSQL"
-  type        = string
-  default     = "dbadmin"
-}
+#variable "db_username" {
+#  description = "Master username for PostgreSQL"
+#  type        = string
+#  default     = "dbadmin"
+#}
 
-variable "db_password" {
-  description = "Master password for PostgreSQL"
-  type        = string
-  sensitive   = true
-}
+#variable "db_password" {
+#  description = "Master password for PostgreSQL"
+#  type        = string
+#  sensitive   = true
+#}
